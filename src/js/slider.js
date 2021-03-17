a974f560cab8babf28e9a04e216fd4537bbb4c0c
NataliYurchenko <nkovalch@gmail.com>
2021-03-17 18:27:32 +0200

    done

---
+++ src/js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        adaptiveHeight:true,
+        infinite:false,
+        easing:'ease',
+        centerMode: true,
+        slidesToShow: 2,
+    
+    });
+})
